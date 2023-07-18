4dfdec6f0b6e774a487a04598b6834147b576717
JhowEight <139798003+JhowEight@users.noreply.github.com>
2023-07-18 14:09:10 -0300

    Add files via upload

---
+++ Aula05MaquinaDeVendas/Form1.Designer.cs
@@ -0,0 +1,231 @@
+﻿namespace Aula05MaquinaDeVendas
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnChocolate = new System.Windows.Forms.Button();
+            this.btnSuco = new System.Windows.Forms.Button();
+            this.btnSalgadinho = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnComprar = new System.Windows.Forms.Button();
+            this.btnCancelar = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.lblTotal = new System.Windows.Forms.Label();
+            this.lblTotalValor = new System.Windows.Forms.Label();
+            this.txtDinheiro = new System.Windows.Forms.TextBox();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // btnChocolate
+            // 
+            this.btnChocolate.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(128)))), ((int)(((byte)(255)))));
+            this.btnChocolate.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnChocolate.Font = new System.Drawing.Font("News706 BT", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnChocolate.Location = new System.Drawing.Point(3, 29);
+            this.btnChocolate.Name = "btnChocolate";
+            this.btnChocolate.Size = new System.Drawing.Size(118, 51);
+            this.btnChocolate.TabIndex = 0;
+            this.btnChocolate.Text = "Chocolate";
+            this.btnChocolate.UseVisualStyleBackColor = false;
+            this.btnChocolate.Click += new System.EventHandler(this.btnChocolate_Click);
+            // 
+            // btnSuco
+            // 
+            this.btnSuco.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(128)))), ((int)(((byte)(255)))));
+            this.btnSuco.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnSuco.Font = new System.Drawing.Font("News706 BT", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnSuco.Location = new System.Drawing.Point(178, 29);
+            this.btnSuco.Name = "btnSuco";
+            this.btnSuco.Size = new System.Drawing.Size(118, 51);
+            this.btnSuco.TabIndex = 1;
+            this.btnSuco.Text = "Suco";
+            this.btnSuco.UseVisualStyleBackColor = false;
+            this.btnSuco.Click += new System.EventHandler(this.btnSuco_Click);
+            // 
+            // btnSalgadinho
+            // 
+            this.btnSalgadinho.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(128)))), ((int)(((byte)(255)))));
+            this.btnSalgadinho.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnSalgadinho.Font = new System.Drawing.Font("News706 BT", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnSalgadinho.Location = new System.Drawing.Point(361, 29);
+            this.btnSalgadinho.Name = "btnSalgadinho";
+            this.btnSalgadinho.Size = new System.Drawing.Size(118, 51);
+            this.btnSalgadinho.TabIndex = 2;
+            this.btnSalgadinho.Text = "Salgadinho";
+            this.btnSalgadinho.UseVisualStyleBackColor = false;
+            this.btnSalgadinho.Click += new System.EventHandler(this.btnSalgadinho_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("News706 BT", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(48, 32);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(449, 32);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Seja bem vindo a lojinha do tio zé";
+            // 
+            // btnComprar
+            // 
+            this.btnComprar.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(128)))), ((int)(((byte)(255)))));
+            this.btnComprar.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnComprar.Font = new System.Drawing.Font("News706 BT", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnComprar.Location = new System.Drawing.Point(119, 214);
+            this.btnComprar.Name = "btnComprar";
+            this.btnComprar.Size = new System.Drawing.Size(118, 51);
+            this.btnComprar.TabIndex = 4;
+            this.btnComprar.Text = "Comprar";
+            this.btnComprar.UseVisualStyleBackColor = false;
+            this.btnComprar.Click += new System.EventHandler(this.btnComprar_Click);
+            // 
+            // btnCancelar
+            // 
+            this.btnCancelar.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(128)))), ((int)(((byte)(255)))));
+            this.btnCancelar.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnCancelar.Font = new System.Drawing.Font("News706 BT", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnCancelar.Location = new System.Drawing.Point(243, 214);
+            this.btnCancelar.Name = "btnCancelar";
+            this.btnCancelar.Size = new System.Drawing.Size(118, 51);
+            this.btnCancelar.TabIndex = 5;
+            this.btnCancelar.Text = "Cancelar";
+            this.btnCancelar.UseVisualStyleBackColor = false;
+            this.btnCancelar.Click += new System.EventHandler(this.btnCancelar_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(30, 83);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(58, 16);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "R$ 2,50";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(219, 83);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(38, 16);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "R$ 7";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(400, 83);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(38, 16);
+            this.label4.TabIndex = 9;
+            this.label4.Text = "R$ 5";
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.txtDinheiro);
+            this.panel1.Controls.Add(this.lblTotalValor);
+            this.panel1.Controls.Add(this.lblTotal);
+            this.panel1.Controls.Add(this.btnChocolate);
+            this.panel1.Controls.Add(this.label4);
+            this.panel1.Controls.Add(this.btnSuco);
+            this.panel1.Controls.Add(this.label3);
+            this.panel1.Controls.Add(this.btnSalgadinho);
+            this.panel1.Controls.Add(this.label2);
+            this.panel1.Controls.Add(this.btnComprar);
+            this.panel1.Controls.Add(this.btnCancelar);
+            this.panel1.Location = new System.Drawing.Point(36, 67);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(483, 507);
+            this.panel1.TabIndex = 10;
+            // 
+            // lblTotal
+            // 
+            this.lblTotal.AutoSize = true;
+            this.lblTotal.Font = new System.Drawing.Font("News706 BT", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblTotal.Location = new System.Drawing.Point(239, 268);
+            this.lblTotal.Name = "lblTotal";
+            this.lblTotal.Size = new System.Drawing.Size(68, 22);
+            this.lblTotal.TabIndex = 10;
+            this.lblTotal.Text = "Total :";
+            // 
+            // lblTotalValor
+            // 
+            this.lblTotalValor.AutoSize = true;
+            this.lblTotalValor.Font = new System.Drawing.Font("News706 BT", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblTotalValor.Location = new System.Drawing.Point(313, 268);
+            this.lblTotalValor.Name = "lblTotalValor";
+            this.lblTotalValor.Size = new System.Drawing.Size(20, 22);
+            this.lblTotalValor.TabIndex = 11;
+            this.lblTotalValor.Text = "0";
+            // 
+            // txtDinheiro
+            // 
+            this.txtDinheiro.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtDinheiro.Location = new System.Drawing.Point(119, 271);
+            this.txtDinheiro.Name = "txtDinheiro";
+            this.txtDinheiro.Size = new System.Drawing.Size(118, 22);
+            this.txtDinheiro.TabIndex = 12;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(552, 487);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnChocolate;
+        private System.Windows.Forms.Button btnSuco;
+        private System.Windows.Forms.Button btnSalgadinho;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnComprar;
+        private System.Windows.Forms.Button btnCancelar;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label lblTotal;
+        private System.Windows.Forms.Label lblTotalValor;
+        private System.Windows.Forms.TextBox txtDinheiro;
+    }
+}
+
---
+++ Aula05MaquinaDeVendas/Form1.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Printing;
+using System.Linq;
+using System.Runtime.Serialization.Formatters;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Aula05MaquinaDeVendas
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnComprar_Click(object sender, EventArgs e)
+        {
+            double total = int.Parse(lblTotalValor.Text);
+
+            int pagar;
+            pagar = int.Parse(txtDinheiro.Text); ;
+            if(pagar >= total)
+            {
+                MessageBox.Show("Compra finalizada!");
+            }
+        }
+
+        private void btnChocolate_Click(object sender, EventArgs e)
+        {
+            double valor = 2.5;
+            double total = double.Parse(lblTotalValor.Text);
+            double soma = valor + total;
+            lblTotalValor.Text = soma.ToString();
+        }
+
+        private void btnSuco_Click(object sender, EventArgs e)
+        {
+            double valor = 7;
+            double total = double.Parse(lblTotalValor.Text);
+            double soma = valor + total;
+            lblTotalValor.Text = soma.ToString();
+        }
+
+        private void btnSalgadinho_Click(object sender, EventArgs e)
+        {
+            double valor = 5;
+            double total = double.Parse(lblTotalValor.Text);
+            double soma = valor + total;
+            lblTotalValor.Text = soma.ToString();
+        }
+
+        private void btnCancelar_Click(object sender, EventArgs e)
+        {
+            lblTotalValor.Text = Convert.ToString("0");
+        }
+    }
+}
